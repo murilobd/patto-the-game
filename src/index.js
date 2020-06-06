96a8f563448f689760d6ec07f0449bb2063cf9ab
Murilo Boareto Delefrate <murilobd@gmail.com>
2020-06-06 20:38:23 -0300

    - added stars - added bomb - added game over - improved char animation

--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@ function preload() {
 	this.load.image("sky", assets.sky);
 	this.load.image("star", assets.star);
 	this.load.image("platform", assets.platform);
-	// this.load.image("star", "assets/star.png");
-	// this.load.image("bomb", "assets/bomb.png");
+	this.load.image("star", assets.star);
+	this.load.image("bomb", assets.bomb);
 	this.load.spritesheet("patto", assets.patto, {
 		frameWidth: 260,
 		frameHeight: 280,
@@ -34,7 +34,12 @@ function preload() {
 
 let platforms;
 let player;
+let stars;
 let cursors;
+let score = 0;
+let scoreText;
+let bombs;
+let gameOver = false;
 
 function create() {
 	this.add.image(400, 300, "sky");
@@ -45,9 +50,15 @@ function create() {
 	platforms.create(50, 250, "platform");
 	platforms.create(750, 220, "platform");
 
-	player = this.physics.add.sprite(100, 50, "patto").setScale(0.3);
+	// player
+	player = this.physics.add
+		.sprite(100, 50, "patto")
+		.setScale(0.3)
+		.setCircle(110, 25, 55);
 	player.setBounce(0.1); // when collide, amount of bounce
 	player.setCollideWorldBounds(true); // it won't let player go off screen
+
+	// player animations
 	this.anims.create({
 		key: "left",
 		frames: this.anims.generateFrameNumbers("patto", { start: 0, end: 1 }),
@@ -56,9 +67,16 @@ function create() {
 	});
 
 	this.anims.create({
-		key: "idle",
+		key: "idleR",
 		frames: this.anims.generateFrameNumbers("patto", { start: 2, end: 3 }),
-		frameRate: 10,
+		frameRate: 3,
+		repeat: -1,
+	});
+
+	this.anims.create({
+		key: "idleL",
+		frames: this.anims.generateFrameNumbers("patto", { start: 8, end: 9 }),
+		frameRate: 3,
 		repeat: -1,
 	});
 
@@ -72,35 +90,53 @@ function create() {
 	this.anims.create({
 		key: "jumpR",
 		frames: [{ key: "patto", frame: 6 }],
-		frameRate: 20,
 	});
 	this.anims.create({
 		key: "jumpL",
 		frames: [{ key: "patto", frame: 7 }],
-		frameRate: 20,
 	});
-
 	this.physics.add.collider(player, platforms); // adds collision between player and platforms
 
+	// stars
+	stars = this.physics.add.group({
+		key: "star",
+		repeat: 11,
+		setXY: { x: 12, y: 0, stepX: 70 },
+	});
+	stars.children.iterate(function (child) {
+		child.setBounceY(Phaser.Math.FloatBetween(0.2, 0.4)); // bounce when star collides with platform randomize between 0.2 and 0.4
+	});
+	this.physics.add.collider(stars, platforms); // add collider between stars and platforms
+	this.physics.add.overlap(player, stars, collectStar, null, this); // when player overlap star, call collectStar function
+
+	// keyboard behaviours
 	cursors = this.input.keyboard.createCursorKeys();
 
-	createButtons();
+	// score
+	scoreText = this.add.text(16, 16, "Score: 0", {
+		fontSize: "32px",
+		fill: "#000",
+	});
+
+	//bombs
+	bombs = this.physics.add.group();
+	this.physics.add.collider(bombs, platforms);
+	this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
 let lastKey = "right";
-let mouseover = null;
 function update() {
-	if (cursors.left.isDown || mouseover === "left") {
+	if (cursors.left.isDown) {
 		lastKey = "left";
 		player.setVelocityX(-160);
 		player.anims.play("left", true);
-	} else if (cursors.right.isDown || mouseover == "right") {
+	} else if (cursors.right.isDown) {
 		lastKey = "right";
 		player.setVelocityX(160);
 		player.anims.play("right", true);
 	} else {
 		player.setVelocityX(0);
-		player.anims.play("idle");
+		player.anims.play(`idle${getFirstLetterLastKey()}`, true);
 	}
 
 	if (cursors.up.isDown && player.body.touching.down) {
@@ -113,32 +149,38 @@ function update() {
 	}
 }
 
-function createButtons() {
-	const mainDiv = document.createElement("div");
-	const buttons = ["left", "right", "up"];
-	const translation = {
-		left: "esquerda",
-		right: "direita",
-		up: "pular",
-	};
-	for (const btn of buttons) {
-		const _btn = document.createElement("button");
-		_btn.id = btn;
-		_btn.innerText = translation[btn];
-		_btn.addEventListener("mouseover", (event) => {
-			const id = event.target.id;
-			if (id === "up") {
-				if (player.body.touching.down) {
-					player.setVelocityY(-330);
-				}
-			} else {
-				mouseover = id;
-			}
+function collectStar(player, star) {
+	star.disableBody(true, true);
+	score += 10;
+	scoreText.setText("Score: " + score);
+
+	if (stars.countActive(true) === 0) {
+		stars.children.iterate(function (child) {
+			child.enableBody(true, child.x, 0, true, true);
 		});
-		_btn.addEventListener("mouseleave", () => {
-			mouseover = null;
-		});
-		mainDiv.append(_btn);
+
+		for (let qttBombs = 0; qttBombs < Math.round(score / 170); qttBombs++) {
+			const x =
+				player.x < 400
+					? Phaser.Math.Between(400, 800)
+					: Phaser.Math.Between(0, 400);
+			const bomb = bombs.create(x, 16, "bomb");
+			bomb.setBounce(1);
+			bomb.setCollideWorldBounds(true);
+			bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+		}
 	}
-	document.body.appendChild(mainDiv);
+}
+
+function hitBomb(player, bomb) {
+	player.angle = 180;
+	this.physics.pause();
+	player.setTint(0xff0000);
+	player.anims.play(`jump${getFirstLetterLastKey()}`);
+	gameOver = true;
+}
+
+function getFirstLetterLastKey() {
+	const _lastKey = lastKey || "r";
+	return _lastKey.substr(0, 1).toUpperCase();
 }
